1b24466707424084353848bbc9fd1606bc8cca3f
Sergiy Stotskiy <s.stotskiy@kwebbl.com>
2013-09-23 18:50:16 +0300

    Updated 'on' binding

--- src/bindings/on.js
+++ src/bindings/on.js
@@ -1,12 +1,31 @@
-require(['ko', 'jquery', 'utils'], function (ko, $, utils) {
+(function (ko, $) {
+  var domData = ko.utils.domData;
+
+  ko.bindingHandlers.on = {
+    init: function (element, valueAccessor, allBindings, viewModel) {
+      var config = valueAccessor(), domNode = $(element);
+
+      for (var rule in config) {
+        if (config.hasOwnProperty(rule)) {
+          var handler = createEventHandlerFor(config, rule);
+          rule = rule.split(/\s+/, 2);
+          if (rule[1]) {
+            domNode.on(rule[0], rule[1], handler);
+            ko.utils.domNodeDisposal.addDisposeCallback(domNode[0], removeEventHandlers);
+          }
+        }
+      }
+    }
+  };
+
   function createValueDefFrom(config) {
     var def;
     if (config.to) {
-      def = [config.to]
+      def = [ config.to ]
     } else if (config.on) {
       def = ['attr', config.on]
-    } else if (config.data) {
-      def = ['data', config.data]
+    } else if (config.data || config.fromData) {
+      def = ['data', (config.data || config.fromData)]
     }
     return def;
   }
@@ -30,31 +49,29 @@ require(['ko', 'jquery', 'utils'], function (ko, $, utils) {
       valueDef   = createValueDefFrom(config);
 
     return function (event) {
-      var
-        context = ko.contextFor(this),
-        method  = utils.lookupMethodIn(context, methodName),
-        result  = method(assembleValueFor(this, valueDef), context.$data, event);
+      var element  = event.currentTarget;
+      var context  = ko.contextFor(this);
+      var scopes   = context.$parents.slice(0);
+      var position = domData.get(element, 'contextHandlerPosition');
+
+      if (!position) {
+        position = _.lookupContextWithMethodPosition(context, methodName);
+        domData.set(element, 'contextHandlerPosition', position);
+      }
+      scopes.unshift(context.$data);
+
+      var scope = scopes[position], result;
+      if (config.passContext) {
+        result = scope[methodName].call(scope, context.$data, context.$parent);
+      } else {
+        result = scope[methodName].call(scope, assembleValueFor(this, valueDef), context, event);
+      }
 
       if (result !== true) { event.preventDefault() }
     };
   }
 
-  ko.bindingHandlers.on = {
-    init: function (element, valueAccessor, allBindings, viewModel) {
-      var config = valueAccessor(), domNode = $(element);
-
-      for (var rule in config) {
-        if (config.hasOwnProperty(rule)) {
-          var handler = createEventHandlerFor(config, rule);
-          rule = rule.split(/\s+/, 2);
-          if (rule[1]) {
-            domNode.on(rule[0], rule[1], handler);
-          } else {
-            domNode.on(rule[0], handler);
-          }
-          ko.utils.domNodeDisposal.addDisposeCallback(domNode[0], utils.removeEventHandlers);
-        }
-      }
-    }
-  };
-});
+  function removeEventHandlers(element) {
+    $(element).off();
+  }
+})(ko, jQuery);
